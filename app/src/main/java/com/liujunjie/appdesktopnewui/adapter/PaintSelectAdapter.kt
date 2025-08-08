fe76daca93ef735b41e8c8dc5b220c6b507cbf99
liujunjie <liujunjie@sanhui.com>
2025-08-08 18:24:57 +0800

    画笔选择弹窗素材添加

--- app/src/main/java/com/liujunjie/appdesktopnewui/adapter/PaintSelectAdapter.kt
+++ app/src/main/java/com/liujunjie/appdesktopnewui/adapter/PaintSelectAdapter.kt
@@ -35,7 +35,24 @@ class PaintSelectAdapter(
     }
 
 
-
+    override fun onBindViewHolder(holder: PaintItemViewHolder, position: Int, payloads: List<Any?>) {
+        if (payloads.isEmpty()) {
+            super.onBindViewHolder(holder, position, payloads)
+        }else{
+            val any = payloads[0] as List<*>
+            if (getItem(position).isSelected){
+               holder. binding.paint.animate()
+                    .translationY(0F)
+                    .setDuration(200)
+                    .start()
+            }else{
+               holder. binding.paint.animate()
+                    .translationY(50F)
+                    .setDuration(200)
+                    .start()
+            }
+        }
+    }
 
     override fun onBindViewHolder(holder: PaintItemViewHolder, position: Int) {
         val item = getItem(position)
@@ -52,8 +69,16 @@ class PaintSelectAdapter(
     private fun updateTranslate(item: PaintItem, holder: PaintItemViewHolder) {
 
     }
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return getItem(position).index.toLong()
+    }
     override fun getItemViewType(position: Int): Int = getItem(position).type.ordinal
-    inner class PaintItemViewHolder(private val binding: SelectPaintItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class PaintItemViewHolder( val binding: SelectPaintItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: PaintItem) {
             binding.paint.setImageResource(item.icon)
             binding.root.setOnClickListener {
@@ -61,13 +86,13 @@ class PaintSelectAdapter(
             }
             if (item.isSelected){
                 Log.d(TAG,"${item.index} 选中了")
-                binding.root.animate()
-                    .translationY(-binding.root.context.dpToPx(25f))
+                binding.paint.animate()
+                    .translationY(-binding.root.context.dpToPx(0F))
                     .setDuration(200)
                     .start()
             }else{
-                Log.d(TAG,"${item.index} 没选中")
-                binding.root.animate()
+                Log.d(TAG,"${item.index} 没选中" +  binding.paint.context.dpToPx(25f))
+                binding.paint.animate()
                     .translationY(binding.root.context.dpToPx(25f))
                     .setDuration(200)
                     .start()
@@ -92,6 +117,14 @@ object PaintDiffCallback : DiffUtil.ItemCallback<PaintItem>() {
     override fun areContentsTheSame(oldItem: PaintItem, newItem: PaintItem): Boolean {
         return oldItem == newItem
     }
+
+    override fun getChangePayload(oldItem: PaintItem, newItem: PaintItem): Any? {
+        if (oldItem.isSelected!= newItem.isSelected) {
+            return listOf("1")
+        }
+        return super.getChangePayload(oldItem, newItem)
+    }
+
 }
 
 
